fd7b25db43f80935c6c3ba8cb30f94e1da3dad00
ayodeji-shote <ayodejishote@hotmail.com>
2023-07-01 17:14:29 +0100

    changes to the food data model

---
+++ FoodStore/Data/ViewModel/FoodListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using FoodStore.Data.Models;
+
+namespace FoodStore.Data.ViewModel
+{
+    public class FoodListViewModel
+    {
+        // A model for the Foods specifically the Food list 
+        // A model to hold the list of Foods 
+        public IEnumerable<Food> Foods { get; set; }
+        // Used to display the current category of Foods 
+        public string CurrentCategory { get; set; }
+
+    }
+}
